5c819177d1252c789e6a8546f5d4dd8d6e2a48f4
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-10-28 04:49:14 +0300

    added return and style

---
+++ src/Api/JsonthemoviedbApi.js
@@ -0,0 +1,38 @@
+// import axios from 'axios';
+
+// const myApiKey = 'e4e77b2d83733ec5f4ee2698cbe57afc';
+
+// export const getMovies = async () => {
+//   const { data } = await axios.get(
+//     `https://api.themoviedb.org/3/trending/movie/day?api_key=${myApiKey}`
+//   );
+//   return data;
+// };
+
+// export const searchMovies = async query => {
+//   const { data } = await axios.get(
+//     `https://api.themoviedb.org/3/search/movie?api_key=${myApiKey}&query=${query}`
+//   );
+//   return data;
+// };
+
+// export const detailsMovies = async id => {
+//   const { data } = await axios.get(
+//     `https://api.themoviedb.org/3/movie/${id}?api_key=${myApiKey}`
+//   );
+//   return data;
+// };
+
+// export const castMovies = async id => {
+//   const { data } = await axios.get(
+//     `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${myApiKey}`
+//   );
+//   return data;
+// };
+
+// export const reviewsMovies = async id => {
+//   const { data } = await axios.get(
+//     `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${myApiKey}`
+//   );
+//   return data;
+// };
--- src/pages/DrinkDetails/DrinkDetails.jsx
+++ src/pages/DrinkDetails/DrinkDetails.jsx
@@ -1,10 +1,111 @@
-import { Container, Title } from './DrinkDetails.styled';
+import {
+  Container,
+  Title,
+  Glass,
+  Description,
+  Button,
+} from './DrinkDetails.styled';
+import data from '../../../data/recipes.json';
+
+// import { useState, useEffect, useRef, Suspense } from 'react';
+// import { Outlet, Link, useParams, useLocation } from 'react-router-dom';
+// import { detailsMovies } from '../Api/JsonthemoviedbApi';
+// import Button from '../button/Button';
+// import ErrorWrapper from 'pages/Error/ErrorWrapper';
+// import css from './MovieDetails.module.css';
+// import Loader from 'components/Loader/Loader';
 
 const DrinkDetails = () => {
+  // const { id } = useParams();
+  // const [movies, setMovies] = useState([]);
+  // const [genres, setGenres] = useState([]);
+  // const [isError, setIsError] = useState(false);
+  // const location = useLocation();
+  // const backLinkHref = useRef(location.state?.from ?? '/');
+  const favorite = true;
+
+  // ***redax
+  // const dispatch = useDispatch();
+  // const handleAddToFavorite = (id) => dispatch(AddFavorite(id));
+  // const handleRemoveToFavorite = (id) => dispatch(RemoveFavorite(id));
+
+  // useEffect(() => {
+  //   async function getMove() {
+  //     try {
+  //       const data = await detailsMovies(id);
+  //       const movies = data;
+  //       setMovies(movies);
+  //       const genres = movies.genres;
+  //       setGenres(genres);
+  //     } catch (error) {
+  //       setIsError(true);
+  //       console.error(error);
+  //     } finally {
+  //     }
+  //   }
+  //   getMove();
+  // }, [id]);
+
+  const { drink, glass, alcoholic, description } = data;
   return (
     <Container>
-      <Title>DrinkDetails</Title>
+      <Title>{drink}drink</Title>
+      <Glass>
+        {glass}Glass / {alcoholic} alcoholic
+      </Glass>
+      <Description>{description} description</Description>
+      {favorite ? (
+        <Button type="button" onClick={() => handleAddToFavorite(id)}>
+          Add to favorite drinks
+        </Button>
+      ) : (
+        <Button type="button" onClick={() => handleRemoveToFavorite(id)}>
+          Remove from favorites
+        </Button>
+      )}
     </Container>
+    // <main>
+    //   <Button to={backLinkHref.current} />
+    //   <ErrorWrapper isError={isError}>
+    //     <div className={css.details}>
+    //       <img
+    //         src={`https://image.tmdb.org/t/p/original${backdrop_path}`}
+    //         alt=""
+    //         height={360}
+    //       />
+    //       <div>
+    //         <h2>{title}</h2>
+    //         <p>{`User Score: ${vote_average}`}</p>
+    //         <h4>Overview</h4>
+    //         <p>{overview}</p>
+    //         <h3>Genres</h3>
+    //         {genres.map(({ id, name }) => (
+    //           <span key={id}>{name}, </span>
+    //         ))}
+    //       </div>
+    //     </div>
+    //     <div className={css.addInform}>
+    //       <h5>Additional information</h5>
+    //       <ul>
+    //         <li>
+    //           <Link to="cast">Cast</Link>
+    //         </li>
+    //         <li>
+    //           <Link to="reviews">Reviews</Link>
+    //         </li>
+    //       </ul>
+    //     </div>
+    //     <Suspense
+    //       fallback={
+    //         <div>
+    //           <Loader />
+    //         </div>
+    //       }
+    //     >
+    //       <Outlet />
+    //     </Suspense>
+    //   </ErrorWrapper>
+    // </main>
   );
 };
 
